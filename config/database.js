df38b3f4f951ac9cfc4b9a168f2f79d991ddb4d4
James Philiossaint <102938926+JPhiliossaint@users.noreply.github.com>
2022-05-30 21:49:30 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://jphiliossaint:resilientcoders@gurrenlagaan.3hsfb.mongodb.net/gurrenlagann?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
+
